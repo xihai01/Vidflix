6908fa9257d9ca687c03af027c84d3ea55b49a8a
xihai01 <xihai.luo01@gmail.com>
2025-03-23 14:34:40 -0400

    implement login route

--- backend/flaskr/auth.py
+++ backend/flaskr/auth.py
@@ -10,31 +10,6 @@ from .schema import User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-'''
-@bp.before_app_request
-def load_logged_in_user():
-  user_id = session.get('user_id')
-
-  if user_id is None:
-    g.user = None
-  else:
-    g.user = get_db().execute(
-      'SELECT * FROM user WHERE id = ?', (user_id,)
-    ).fetchone()
-'''
-
-'''
-def login_required(view):
-  @functools.wraps(view)
-  def wrapped_view(**kwargs):
-    if g.user is None:
-      return redirect(url_for('auth.login'))
-
-    return view(**kwargs)
-
-  return wrapped_view'
-'''
-
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
   if request.method == 'POST':
@@ -44,8 +19,6 @@ def register():
     email_regex = r'^\S+@\S+$'
     images = ['/avatar1.png', '/avatar2.png', '/avatar3.png']
     profile_pic = images[random.randint(0, 2)]
-    #db = get_db()
-    #error = None
 
     if not username:
       return jsonify({'message': 'The field username is required.'}), 400
@@ -77,20 +50,6 @@ def register():
     access_token = create_access_token(identity=new_user.id)
     new_user.save()
 
-    #if error is None:
-      #try:
-        #db.execute(
-         # 'INSERT INTO user (username, password) VALUES (?, ?)',
-          #(username, generate_password_hash(password))
-        #)
-        #db.commit()
-      #except db.IntegrityError:
-        #error = f"User {username} is already registered."
-      #else:
-        #return redirect(url_for('auth.login'))
-
-      #flash(error)
-
     response_obj = {
       "username": new_user.username,
       "email": new_user.email,
@@ -103,33 +62,32 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
   if request.method == 'POST':
-    #username = request.form['username']
-    #password = request.form['password']
-    #db = get_db()
-    #error = None
-    #user = db.execute(
-     # 'SELECT * FROM user WHERE username = ?', (username,)
-    #).fetchone()
-    '''
-    if user is None:
-      error = 'Incorrect username.'
-    elif not check_password_hash(user['password'], password):
-      error = 'Incorrect password.'
+    username = request.form['username']
+    password = request.form['password']
 
-    if error is None:
-      session.clear()
-      session['user_id'] = user['id']
-      return redirect(url_for('index'))
+    if not username or not password:
+      return jsonify({'message': 'All fields are required.'}), 400
 
-    flash(error)'
-    '''
-    return jsonify({'message': 'login route'})
+    user = User.objects(username=username).first()
+    if not user:
+      return jsonify({'message': 'Invalid credentials.'}), 400
+
+    if not check_password_hash(user.password, password):
+      return jsonify({'message': 'Invalid credentials.'}), 400
+
+    access_token = create_access_token(identity=user.id)
+    response_obj = {
+      "username": user.username,
+      "email": user.email,
+      "image": user.image,
+      "access_token": access_token
+    }
+
+    return jsonify({'message': 'User logged in successfully.', "user": response_obj}), 200
 
 @bp.route('/logout')
 @jwt_required()
 def logout():
-  #session.clear()
-  #return redirect(url_for('index'))
   user = get_jwt_identity()
   if User.objects(id=user).first():
     return jsonify({'message': 'User logged out successfully.', 'current_user': user}), 200
