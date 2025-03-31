e0ce915dc51679426d907fb7649324a11511ca99
xihai01 <xihai.luo01@gmail.com>
2025-03-31 07:47:21 -0400

    update sign out page to pre-populate email field if provided by user

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ChevronRight } from "lucide-react";
 
 const AuthScreen = () => {
   const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    navigate("/signup?email=" + email);
+  }
+
   return (
     <div className="hero-bg reltive">
       {/* navbar */}
@@ -27,7 +34,7 @@ const AuthScreen = () => {
         <p className="mb-4">
           Ready to watch? Enter your email to create or restart your membership.
         </p>
-        <form className="flex flex-col md:flex-row gap-4 w-1/2">
+        <form className="flex flex-col md:flex-row gap-4 w-1/2" onSubmit={handleFormSubmit}>
           <input
             type="email"
             placeholder="Email address"
